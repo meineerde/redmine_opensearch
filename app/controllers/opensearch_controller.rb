4aee2c1d77f7452ae27dc88e3b424c93935540b5
Holger Just <redmine@meine-er.de>
2010-04-28 08:48:02 +0000

    Enable browser to access the global opensearch plugin without the need to authenticate.
    git-svn-id: http://dev.holgerjust.de/svn/redmine-opensearch/trunk@8 c013d87b-c2fd-cfd9-8227-e182169ea283

--- app/controllers/opensearch_controller.rb
+++ app/controllers/opensearch_controller.rb
@@ -3,10 +3,15 @@ class OpensearchController < ApplicationController
   
   before_filter :find_optional_project
   
+  # only perform access checks if we have a @project
+  # global opensearch plugin is safe
+  skip_before_filter :check_if_login_required
+  before_filter :check_project_privacy, :only => Proc.new {@project}
+  
   def index
     render :index, :layout => false, :content_type => 'application/opensearchdescription+xml'
   end
-  
+
   def find_optional_project
     @project = Project.find(params[:project_id]) unless params[:project_id].blank?
   rescue ActiveRecord::RecordNotFound
