3b10670441867aeb8a0272c677b4812d56a277c3
Holger Just <redmine@meine-er.de>
2010-05-18 17:02:57 +0000

    insert the favicon as data uri instead of a simple pointer if possible.
    git-svn-id: http://dev.holgerjust.de/svn/redmine-opensearch/trunk@14 c013d87b-c2fd-cfd9-8227-e182169ea283

--- app/helpers/opensearch_favicon_helper.rb
+++ app/helpers/opensearch_favicon_helper.rb
@@ -6,13 +6,35 @@ module OpensearchFaviconHelper
     # this is implemented in the redmine_favicon plugin
     include FaviconHelper
   rescue
-    favicon_path = "/favicon.ico"
+    favicon_filesystem_path = File.join(ActionViewHelper::ASSETS_DIR, 'favicon.ico')
+  end
+  
+  def favicon_data_uri
+    if favicon_filesystem_path.nil?
+      favicon_url
+    elsif File.exist?(favicon_filesystem_path)
+      @favicon_data_uri ||= begin
+        contents = File.read(favicon_filesystem_path)
+        base64 = Base64::encode64(contents).gsub("\n", "")
+        # TODO: determine the correct mime type
+        mime_type=""
+      
+        "data:#{mime_type};base64,#{base64}"
+      end
+    end
   end
   
   def favicon_url
-    favicon = URI.parse favicon_path
-    favicon.host = Setting.host_name unless favicon.host
-    favicon.scheme = Setting.protocol unless favicon.scheme
-    favicon.to_s
+    @favicon_url ||= begin
+      favicon = URI.parse favicon_path
+    
+      (host_name, color, port) = Setting.host_name.partition(":")
+    
+      favicon.host = host_name unless favicon.host
+      favicon.port = port unless (favicon.port || port.blank?)
+      favicon.scheme = Setting.protocol unless favicon.scheme
+      favicon.to_s
+    end
   end
+  
 end
